630cad9fb00b39aac865243bb4190706c0a092e4
Raj Bobra <rajbobra.py@gmail.com>
2025-04-02 16:44:27 -0700

    Refactored File organizer and added error handling

--- src/file_organizer.py
+++ src/file_organizer.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import os
 import shutil
 
@@ -10,43 +11,38 @@ class FileOrganizer:
 
     
     def get_files(self):
-        file_map = {}
+        file_map = defaultdict(list)
         for dirpath,_,files in os.walk(self.path):
             for file in files:
                 ext = os.path.splitext(file)[1]
-                full_path = os.path.join(dirpath, file)
-                if ext in file_map:
-                    file_map[ext].append(full_path)
-                else:
-                    file_map[ext] = [full_path]
+                file_map[ext].append(os.path.join(dirpath, file))
             break # only scan current level
         
         return file_map
+    
+
+    def ensure_directory(self, target_dir_name):
+        target_dir = os.path.join(self.path, target_dir_name)
+        if not os.path.exists(target_dir):
+            try:
+                os.mkdir(target_dir)
+                self.logger.info(f'Created Directory: "{target_dir}" as it did not exist')
+            except PermissionError:
+                self.logger.info(f'Permission Denied: Failed to create "{target_dir}"')
+                raise
+            except Exception as e:
+                self.logger.info(f'Failed to create "{target_dir}": {e}')
+                raise
+        return target_dir
+
 
     def organize(self):
         file_map = self.get_files()
-        # go through the file map
-            # if key (ext) exists in rule map
-                # create rule dir if it does not exist
-                # move all files to this dir
-            # else create 'Other' dir if does not exist
-                # add the files to Other
         for ext in file_map:
-            if ext in self.rules:
-                # create rule dir if it does not exist
-                dir = f'{self.path}/{self.rules[ext]}'
-                if not os.path.exists(dir):
-                    os.mkdir(dir)
-                    self.logger.info(f'Created Directory: "{self.rules[ext]}" as it did not exist')
-                for file in file_map[ext]:
-                    shutil.move(file, dir)
-                    self.logger.info(f'Moved: {file} --> {dir}')
-                # move all files to this dir
-            else:
-                dir = f'{self.path}/Other'
-                if not os.path.exists(dir):
-                    self.logger.info(f'Created Directory: "Other" as it did not exist')
-                    os.mkdir(dir)
-                for file in file_map[ext]:
-                    shutil.move(file, dir)
-                    self.logger.info(f'Moved: {file} --> {dir}')
+            target_dir = self.ensure_directory(self.rules[ext]) if ext in self.rules else self.ensure_directory('Other')
+            for file in file_map[ext]:
+                try:
+                    shutil.move(file, target_dir)
+                    self.logger.info(f'Moved: {file} --> {target_dir}')
+                except Exception as e:
+                    self.logger.error(f'Failed to move {file} to {target_dir}: {e}')
